de0eb3043dcb6562ed94440867022fbc531b26a6
Lenin <ingeleninfernando@gmail.com>
2023-05-10 20:56:40 -0500

    task functionality

--- src/App.js
+++ src/App.js
@@ -3,24 +3,53 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+
+ constructor() {
+    super()
+    this.state = {
+      tasks: [
+        "Sacar la ropa", "Hacer la cama", "Leer un rato"
+      ],
+      newTask: ''
+    }
+  }
+
+  handleSubmit(e) {
+    
+    e.preventDefault();
+        this.setState({newTask: e.target.value});
+        this.addTask(this.state.newTask)
+    }
+  
+
+  handleChange(e) {
+    this.setState({newTask: e.target.value});
+  }
+
+  addTask(dato) {
+    this.setState({tasks:this.state.tasks.concat(this.state.newTask)})
+    console.log(dato)
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.tasks.map((task, index) => <li key={index}>{task}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask || ""}  // Only for "Enter" purpose
+           onChange={this.handleChange.bind(this)} />
+          </form>
         </div>
+        <span>{this.state.newTask}</span>
       </div>
     )
   }
 }
+
 
 
 export default App;
